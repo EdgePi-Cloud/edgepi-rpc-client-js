6a59d1d311c8fa8c88e9ac1816b102fa4d99ee9d
iaj2 <isaacjames124@yahoo.ca>
2023-07-18 19:24:47 +0100

    custom types

---
+++ src/types/types.ts
@@ -0,0 +1,32 @@
+import type * as protobuf from 'protobufjs'
+
+/*
+NOTE: protobufjs requries that:
+- When messages are created by protobufjs, all fields are converted to camelCase convention.
+  i.e messages with fields like "abc_yo" defined in a .proto file will take be converted to "abcYo"
+*/
+
+export interface serviceRequest {
+  serviceName: string
+  methodName: string
+  requestMsg: object
+}
+
+export interface RpcRequest extends protobuf.Message {
+  serviceName?: string
+  methodName?: string
+  responseProto?: Uint8Array
+}
+
+export interface RpcResponse extends protobuf.Message {
+  serviceName?: string
+  methodName?: string
+  responseProto?: Uint8Array | protobuf.Reader
+  errorMsg?: string
+  errorCode?: number
+}
+
+export interface serverResponse {
+  error: string | undefined
+  content: protobuf.Message | undefined
+}
