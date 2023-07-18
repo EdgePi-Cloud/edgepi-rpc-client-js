6a683790b4c85c3d93d49275c0fb5bf6fa2874ea
iaj2 <isaacjames124@yahoo.ca>
2023-07-18 21:38:29 +0100

    update tests after restructure

--- tests/unit/RpcChannel.test.js
+++ tests/unit/RpcChannel.test.js
@@ -32,100 +32,127 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// Import required modules and types
 const protobuf = __importStar(require("protobufjs"));
-const RpcChannel_1 = require("../../RpcChannel");
+const RpcChannel_1 = require("../../src/rpcChannel/RpcChannel");
+// Test suite for RpcChannel class
 describe('RpcChannel', () => {
+    // Declare variables to be used in tests
     let socketEndpoint;
     let protoRoot;
     let rpcChannel;
+    let mockedMsgType;
+    // Set up before each test
     beforeEach(() => {
+        // Initialize test data
         socketEndpoint = 'fake_endpoint';
         protoRoot = new protobuf.Root();
-        // Mock type lookup
-        const mockedMsgType = {
+        // Mock protobuf.Type to be used in RpcChannel
+        mockedMsgType = {
             create: jest.fn(),
-            encode: jest.fn().mockReturnValueOnce({
-                finish: jest.fn().mockReturnValueOnce({})
-            })
+            encode: jest.fn().mockReturnValueOnce({ finish: jest.fn().mockReturnValueOnce({}) }),
+            decode: jest.fn()
         };
-        jest.spyOn(protoRoot, 'lookupType').mockReturnValueOnce(mockedMsgType);
+        // Mock the 'lookupType' method in protoRoot to return mockedMsgType
+        jest.spyOn(protoRoot, 'lookupType').mockReturnValue(mockedMsgType);
+        // Initialize RpcChannel instance with the mock data
         rpcChannel = new RpcChannel_1.RpcChannel(socketEndpoint, protoRoot);
     });
+    // Clean up after each test
     afterEach(() => {
         jest.resetAllMocks();
     });
-    // Test class init
+    // Test case: RpcChannel instance should be defined
     it('should be defined', () => {
         expect(rpcChannel).toBeDefined();
     });
+    // Test suite for createRpcRequest method
     describe('createRpcRequest', () => {
-        it('should throw an error if method parent is null', () => {
-            const method = { parent: null, name: 'SomeMethod' };
-            const requestData = new Uint8Array();
-            expect(() => rpcChannel.createRpcRequest(method, requestData)).toThrowError('Method parent is null. Could not find requested service.');
-        });
+        // Test case: createRpcRequest should create and return an RpcRequest message
         it('should create and return an RpcRequest message', () => {
-            const serviceName = 'SomeService';
-            const methodName = 'SomeMethod';
-            const requestData = new Uint8Array();
-            const createSpy = jest.spyOn(rpcChannel.rpc_request_type, 'create').mockReturnValueOnce({});
-            const rpcRequest = rpcChannel.createRpcRequest({ parent: { name: serviceName }, name: methodName }, requestData);
-            expect(createSpy).toHaveBeenCalledWith({
-                serviceName: serviceName,
-                methodName: methodName,
-                requestProto: requestData,
+            // Arrange mocks
+            const mockRequest = {};
+            const mockRpcRequest = {};
+            const requestCreateSpy = jest.spyOn(mockedMsgType, 'create').mockReturnValueOnce(mockRequest);
+            const rpcRequestCreateSpy = jest.spyOn(rpcChannel.rpcRequestType, 'create').mockReturnValueOnce(mockRpcRequest);
+            // Call createRpcRequest with mock data
+            const mockServiceReq = { serviceName: 'service', methodName: 'method', requestMsg: {} };
+            const rpcRequest = rpcChannel.createRpcRequest(mockServiceReq, mockedMsgType);
+            // Assertions
+            expect(requestCreateSpy).toHaveBeenCalledWith(mockServiceReq.requestMsg);
+            expect(mockedMsgType.encode).toHaveBeenCalledWith(mockRequest);
+            expect(rpcRequestCreateSpy).toHaveBeenCalled();
+            expect(rpcRequest).toEqual(mockRpcRequest);
+        });
+    });
+    // Test suite for sendRpcRequest method
+    describe('sendRpcRequest', () => {
+        // Test case: sendRpcRequest should encode and send the RPC request over the socket
+        it('should encode and send the RPC request over the socket', () => __awaiter(void 0, void 0, void 0, function* () {
+            // Arrange mocks
+            const rpcRequest = {};
+            const sendSpy = jest.spyOn(rpcChannel.socket, 'send').mockResolvedValueOnce(undefined);
+            // call sendRpcRequest
+            yield rpcChannel.sendRpcRequest(rpcRequest);
+            // Assertions
+            expect(rpcChannel.rpcRequestType.encode).toHaveBeenCalledWith(rpcRequest);
+            expect(sendSpy).toHaveBeenCalledWith({});
+        }));
+    });
+    // Test suite for getRpcResponse method
+    describe('getRpcResponse', () => {
+        // Test case: getRpcResponse should receive and return the RPC response from the server
+        it('should receive and return the RPC response from the server', () => __awaiter(void 0, void 0, void 0, function* () {
+            // Arrange mocks
+            const mockRpcResponseData = new Uint8Array();
+            const mockRpcResponse = {};
+            const receiveSpy = jest.spyOn(rpcChannel.socket, 'receive').mockResolvedValueOnce([mockRpcResponseData]);
+            const decodeSpy = jest.spyOn(rpcChannel.rpcRequestType, 'decode').mockReturnValue(mockRpcResponse);
+            // Call getRpcResponse
+            const response = yield rpcChannel.getRpcResponse();
+            // Assertions
+            expect(receiveSpy).toHaveBeenCalled();
+            expect(decodeSpy).toHaveBeenCalledWith(mockRpcResponseData);
+            expect(response).toBe(mockRpcResponse);
+        }));
+    });
+    // Test suite for createServerResponse method
+    describe('createServerResponse', () => {
+        // Test case 1: createServerResponse should return the response object with content when there is no error and valid responseProto
+        it('should return the response object with content when there is no error and valid responseProto', () => {
+            // Arrange mocks
+            const mockRpcResponse = {
+                responseProto: Buffer.from('protobuf_response_data'),
+                errorCode: undefined,
+                errorMsg: undefined
+            };
+            const mockServerResponseMsg = { someField: 'someValue' };
+            const decodeSpy = jest.spyOn(mockedMsgType, 'decode').mockReturnValueOnce(mockServerResponseMsg);
+            // Call the createServerResponse with the mocked objects
+            const result = rpcChannel.createServerResponse(mockedMsgType, mockRpcResponse);
+            // Assertions
+            expect(decodeSpy).toHaveBeenCalledWith(mockRpcResponse.responseProto);
+            expect(result).toEqual({
+                error: undefined,
+                content: mockServerResponseMsg
             });
-            expect(rpcRequest).toEqual({});
         });
-        describe('sendRpcRequest', () => {
-            it('should encode and send the RPC request over the socket', () => __awaiter(void 0, void 0, void 0, function* () {
-                const rpcRequest = {};
-                const sendSpy = jest.spyOn(rpcChannel.socket, 'send').mockResolvedValueOnce(undefined);
-                yield rpcChannel.sendRpcRequest(rpcRequest);
-                expect(rpcChannel.rpc_request_type.encode).toHaveBeenCalledWith(rpcRequest);
-                expect(sendSpy).toHaveBeenCalledWith({});
-            }));
-        });
-        describe('getRpcResponse', () => {
-            it('should receive and return the RPC response from the server', () => __awaiter(void 0, void 0, void 0, function* () {
-                const rpcResponseData = new Uint8Array();
-                const receiveSpy = jest.spyOn(rpcChannel.socket, 'receive').mockResolvedValueOnce([rpcResponseData]);
-                const response = yield rpcChannel.getRpcResponse();
-                expect(receiveSpy).toHaveBeenCalled();
-                expect(response).toBe(rpcResponseData);
-            }));
-        });
-        describe('callMethod', () => {
-            let method;
-            let requestData;
-            let callback;
-            beforeEach(() => {
-                method = { parent: { name: 'SomeService' }, name: 'SomeMethod' };
-                requestData = new Uint8Array();
-                callback = jest.fn();
+        // Test case 2: createServerResponse should return the response object with an error message when errorCode and errorMsg are provided
+        it('should return the response object with an error message when errorCode and errorMsg are provided', () => {
+            // Mock the RpcResponse object
+            const mockRpcResponse = {
+                responseProto: undefined,
+                errorCode: 0,
+                errorMsg: 'Bad message data'
+            };
+            // Call the createServerResponse with the mocked objects
+            const result = rpcChannel.createServerResponse(mockedMsgType, mockRpcResponse);
+            // Assertions
+            expect(mockedMsgType.decode).not.toHaveBeenCalled();
+            expect(result).toEqual({
+                error: 'Error 0: Bad message data',
+                content: undefined
             });
-            it('should create an RPC request, send it, and invoke the callback with the response', () => __awaiter(void 0, void 0, void 0, function* () {
-                const rpcRequest = {};
-                const rpcResponse = new Uint8Array();
-                // Mock rpcChannel methods within callMethod
-                const createRpcRequestSpy = jest.spyOn(rpcChannel, 'createRpcRequest').mockReturnValueOnce(rpcRequest);
-                const sendRpcRequestSpy = jest.spyOn(rpcChannel, 'sendRpcRequest').mockResolvedValueOnce(undefined);
-                const getRpcResponseSpy = jest.spyOn(rpcChannel, 'getRpcResponse').mockResolvedValueOnce(rpcResponse);
-                yield rpcChannel.callMethod(method, requestData, callback);
-                expect(createRpcRequestSpy).toHaveBeenCalledWith(method, requestData);
-                expect(sendRpcRequestSpy).toHaveBeenCalledWith(rpcRequest);
-                expect(getRpcResponseSpy).toHaveBeenCalled();
-                expect(callback).toHaveBeenCalledWith(null, rpcResponse);
-            }));
-            it('should invoke the callback with an error if an exception occurs', () => __awaiter(void 0, void 0, void 0, function* () {
-                const error = new Error('Some error');
-                const createRpcRequestSpy = jest.spyOn(rpcChannel, 'createRpcRequest').mockImplementation(() => {
-                    throw error;
-                });
-                yield rpcChannel.callMethod(method, requestData, callback);
-                expect(createRpcRequestSpy).toHaveBeenCalledWith(method, requestData);
-                expect(callback).toHaveBeenCalledWith(error);
-            }));
         });
     });
 });
