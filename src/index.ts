e18e85968833d5a8d7b4323f0a70992db4ef58b0
iaj2 <isaacjames124@yahoo.ca>
2023-07-25 23:04:56 +0100

    integration tests

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { TcService } from './services/TcService/TcService'
+import { LEDService } from './services/LedService/LedService'
 import { LEDPins } from './services/LedService/LedPins'
+
 
 export {
   TcService,
+  LEDService,
   LEDPins
 }
