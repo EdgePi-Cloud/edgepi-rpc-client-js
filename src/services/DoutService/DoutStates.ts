90ccccfcd23bf15aad607d267d3073ae6438c8c7
iaj2 <isaacjames124@yahoo.ca>
2023-09-03 11:47:56 -0700

    fix proto path

--- src/services/DoutService/DoutStates.ts
+++ src/services/DoutService/DoutStates.ts
@@ -2,7 +2,7 @@ import * as protobuf from 'protobufjs'
 import path from 'path'
 import { DoutTriStates } from './DoutTypes';
 
-const protoPckgPath = path.join(process.cwd(), 'node_modules', '@edgepi-cloud', 'rpc-protobuf');
+const protoPckgPath = path.join(require.resolve('@edgepi-cloud/rpc-protobuf'), '..');
 const root = protobuf.loadSync(path.join(protoPckgPath, 'dout.proto'))
 const DoutTriState = root.lookupEnum('DoutTriState').values as unknown as DoutTriStates
 
